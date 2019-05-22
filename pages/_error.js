61b1c692e7a2f7e8eefbfcc9b9de98d0cf95762a
Viacheslav Yepancha <viacheslav.yepancha@vantino.ch>
2019-05-22 18:00:33 +0300

    redirect to dashboard

---
+++ pages/_error.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+    render() {
+        return (
+            <h1>
+                Error!
+            </h1>
+        );
+    }
+}
+
+export default Error;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function Home() {
-    return <div>Welcome to Next.js!</div>;
+import {Component} from 'react';
+import Router from 'next/router';
+
+class Home extends Component {
+    static async getInitialProps(ctx) {
+        let homepage = '/dashboard';
+        if (ctx && ctx.req) {
+            ctx.res.writeHead(302, {Location: homepage})
+            ctx.res.end()
+        } else {
+            Router.push(homepage)
+        }
+    }
 }
 
 export default Home;
