ea2eb00005b73ab0b9d7a99a6f5b19f8c60cb1e2
Viacheslav Yepancha <viacheslav.yepancha@vantino.ch>
2019-06-12 17:52:37 +0300

    OnPage component

---
+++ components/Pagination/OnPage.js
@@ -0,0 +1,18 @@
+//@flow
+import React from 'react';
+
+type Props = {
+    onChange: Function,
+    options: Array,
+};
+function OnPage(props: Props) {
+    return (
+        <select name="onPage" id="onPage" onChange={e => props.onChange(e.target.value)} className={'select'}>
+            {props.options.map(option => (
+                <option value={option}>{option}</option>
+            ))}
+        </select>
+    );
+}
+
+export default OnPage;
