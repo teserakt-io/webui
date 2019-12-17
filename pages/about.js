34a3851dcd42675d76dfd3c47b036023fb1fca38
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2019-12-17 17:35:49 +0100

    about draft

--- pages/about.js
+++ pages/about.js
@@ -5,6 +5,58 @@ class About extends React.Component<{}> {
         return (
             <React.Fragment>
                 <h1>About</h1>
+                <p>
+                    This application is a demonstration version of the web
+                    console of <a href="https://teserakt.io/#e4">E4</a>,
+                    Teserakt's IoT data protection solution.
+                </p>
+                <p>
+
+                    E4 consists of the:
+                    <ul>
+                    <li>
+                    <b>E4 client library</b>, available on <a href="https://github.com/teserakt-io">our GitHub</a> under APLv2 license
+                    </li>
+                    <li>
+                    <b>Command-and-control (C2) server</b>, offered
+                    under commercial terms including maintenance and
+                    support.
+                    </li>
+                    </ul>
+                    Detailed information about E4 is available on our <a
+                    href="https://e4.teserakt.io">documentation
+                    site</a>.
+                </p>
+                <p>
+                    Please feel free to contact us at <a
+                    href="mailto:contact@teserakt.io">contact@teserakt.io</a> for any inquiry.
+                </p>
+                <h2>Using the console</h2>
+                <p>
+                    You can create an MQTT client supporting E4 by
+                    following the instructions in our <a
+                    href="https://github.com/teserakt-io/e4go">client software repository</a>.
+                    The client must use the broker mqtt.demo.teserakt.io:1883, and
+                    must subscribe to its control topic (<code>e4/devicename</code>).
+                </p>
+                <p>
+                    After registering your client in the console ("Add
+                    Client" button) you can use the consol to assign
+                    topic keys to your device, and to define key
+                    rotation policies.
+                </p>
+                <h2>Disclaimer</h2>
+                <p>                
+                    This demonstration application is open to anyone
+                    without identification or authentication. 
+                    We provide no availability guarantees nor privacy
+                    guarantees (all the content entered in the
+                    application will be visible to other users).
+                    Teserakt is not liable for any loss or damage
+                    related to the usage of the application.
+                    Teserakt is not liable for any content which may be
+                    considered offensive.
+                </p>                
             </React.Fragment>
         )
     }
