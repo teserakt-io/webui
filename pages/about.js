1af9e9f358bb3ea3272278f0866da412efc35d1c
Flavien binet <flavien.binet@gmail.com>
2019-12-23 15:46:14 +0100

    Fixed markup warning on about page

--- pages/about.js
+++ pages/about.js
@@ -10,35 +10,34 @@ class About extends React.Component<{}> {
                     console of <a href="https://teserakt.io/#e4">E4</a>,
                     Teserakt's IoT data protection solution.
                 </p>
-                <p>
+                <div>
 
                     E4 consists of the:
                     <ul>
-                    <li>
-                    <b>E4 client library</b>, available on <a
-                    href="https://github.com/teserakt-io">our GitHub</a>
-                    under the APLv2 license.
-                    </li>
-                    <li>
-                    <b>Command-and-control (C2) server</b>, offered
-                    under commercial terms including maintenance and
-                    support.
-                    </li>
+                        <li>
+                            <b>E4 client library</b>, available on <a
+                                href="https://github.com/teserakt-io">our GitHub</a> under the APLv2 license.
+                        </li>
+                        <li>
+                            <b>Command-and-control (C2) server</b>, offered
+                            under commercial terms including maintenance and
+                            support.
+                        </li>
                     </ul>
                     Detailed information about E4 is available on our <a
-                    href="https://e4.teserakt.io">documentation
+                        href="https://e4.teserakt.io">documentation
                     site</a>.
-                </p>
+                </div>
                 <p>
                     For all enquiries, please contact us at <a
-                    href="mailto:contact@teserakt.io">contact@teserakt.io</a>.
+                        href="mailto:contact@teserakt.io">contact@teserakt.io</a>.
                 </p>
                 <h2>Using the console</h2>
                 <p>
                     You can create an MQTT client supporting E4 by
                     following the instructions in our <a
-                    href="https://github.com/teserakt-io/e4go"> client software repository</a>.
-                    The client must use the broker mqtt.demo.teserakt.io:1883, and
+                        href="https://github.com/teserakt-io/e4go"> client software repository</a>.
+    The client must use the broker mqtt.demo.teserakt.io:1883, and
                     must subscribe to its control topic (<code>e4/devicename</code>).
                 </p>
                 <p>
@@ -48,9 +47,9 @@ class About extends React.Component<{}> {
                     rotation policies.
                 </p>
                 <h2>Disclaimer</h2>
-                <p>                
+                <p>
                     This demonstration application is open to anyone
-                    without identification or authentication. 
+                    without identification or authentication.
                     We provide no availability guarantees nor privacy
                     guarantees (all the content entered in the
                     application will be visible to other users).
@@ -58,7 +57,7 @@ class About extends React.Component<{}> {
                     related to the usage of the application.
                     Teserakt is not liable for any content which may be
                     considered offensive.
-                </p>                
+                </p>
             </React.Fragment>
         )
     }
